5a7916357c39a89b2fa3b71c5e3f15abba2a5068
benaiah-ke <benaiahpraise@gmail.com>
2022-11-09 15:49:45 +0300

    landing, select bus, routes and tickets js and css

---
+++ src/pages/selectbus.js
@@ -0,0 +1,170 @@
+import "./SelectBus.css";
+
+export const SelectBus = ({ ...props }) => {
+  return (
+    <div className="select-bus">
+      <div className="bg"></div>
+      <div className="image-1"></div>
+      <div className="ellipse-733"></div>
+      <div className="ellipse-734"></div>
+      <div className="rectangle-180"></div>
+      <div className="rectangle-9"></div>
+      <div className="book-now">Book Now</div>
+      <div className="departure-time">Departure Time</div>
+      <div className="from-nairobi-bus-station">from Nairobi Bus Station</div>
+      <div className="select-one-of-the-buses-plying-your-route">
+        Select one of the buses plying your route
+      </div>
+      <div className="texts">
+        <div className="_8-30-am">8:30 AM</div>
+      </div>
+      <div className="texts">
+        <div className="ksh-1-500">Ksh 1,500</div>
+      </div>
+      <div className="logo">
+        <div className="ma3pass">
+          <span>
+            <span className="ma3pass-span">ma3</span>
+            <span className="ma3pass-span2">Pass</span>
+          </span>
+        </div>
+      </div>
+      <div className="menu">
+        <div className="rectangle-121"></div>
+        <div className="routes">Routes</div>
+        <div className="how-it-works">How it Works</div>
+        <div className="home">Home</div>
+        <div className="my-tickets">My Tickets</div>
+      </div>
+      <div className="button">
+        <div className="rectangle-92"></div>
+        <div className="my-account">My Account</div>
+      </div>
+      <div className="image-14"></div>
+      <div className="texts2">
+        <div className="easycoach-buses">EasyCoach Buses</div>
+      </div>
+      <div className="line-2"></div>
+      <div className="rectangle-181"></div>
+      <div className="rectangle-182"></div>
+      <div className="book-now2">Book Now</div>
+      <div className="departure-time2">Departure Time</div>
+      <div className="from-nairobi-bus-station2">from Nairobi Bus Station</div>
+      <div className="texts">
+        <div className="_9-30-am">9:30 AM</div>
+      </div>
+      <div className="image-15"></div>
+      <div className="easycoach-buses2">EasyCoach Buses</div>
+      <div className="texts3">
+        <div className="other-routes">Other Routes</div>
+      </div>
+      <svg
+        className="line-1"
+        width="222"
+        viewBox="0 0 222 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0 0H222" stroke="black" strokeWidth="5" />
+      </svg>
+      <svg
+        className="arrow-5"
+        width="20"
+        viewBox="0 0 20 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M20.7071 0.707107C21.0976 0.316583 21.0976 -0.316583 20.7071 -0.707107L14.3431 -7.07107C13.9526 -7.46159 13.3195 -7.46159 12.9289 -7.07107C12.5384 -6.68054 12.5384 -6.04738 12.9289 -5.65685L18.5858 0L12.9289 5.65685C12.5384 6.04738 12.5384 6.68054 12.9289 7.07107C13.3195 7.46159 13.9526 7.46159 14.3431 7.07107L20.7071 0.707107ZM0 1H20V-1H0V1Z"
+          fill="black"
+        />
+      </svg>
+      <div className="line-3"></div>
+      <div className="texts">
+        <div className="ksh-1-300">Ksh 1,300</div>
+      </div>
+      <div className="rectangle-183"></div>
+      <div className="rectangle-184"></div>
+      <div className="book-now3">Book Now</div>
+      <div className="departure-time3">Departure Time</div>
+      <div className="from-nairobi-bus-station3">from Nairobi Bus Station</div>
+      <div className="_7-30-pm">7:30 PM</div>
+      <div className="group-17558">
+        <div className="bg2"></div>
+        <div className="group-17556">
+          <div className="ellipse-7332"></div>
+          <div className="ellipse-7342"></div>
+        </div>
+      </div>
+      <div className="rectangle-179"></div>
+      <div className="term---condition">Term &amp; Condition</div>
+      <div className="privacy---policy">Privacy &amp; Policy</div>
+      <div className="ma3pass-2022">MA3PASS 2022</div>
+      <div className="menu-3">
+        <div className="social">Social</div>
+        <div className="instagram">Instagram</div>
+        <div className="facebook">Facebook</div>
+        <div className="twitter">Twitter</div>
+        <div className="linkedin">Linkedin</div>
+      </div>
+      <div className="menu-1">
+        <div className="menu2">Menu</div>
+        <div className="home2">Home</div>
+        <div className="how-it-works2">How it works</div>
+        <div className="routes2">Routes</div>
+        <div className="my-tickets2">My Tickets</div>
+      </div>
+      <div className="group-458">
+        <div className="search--book-and-pay-for-your-matatu-ticket-the-easy-way-with-ma3pass--travel-with-convenience">
+          Search, book and pay for your matatu ticket the easy way with Ma3Pass.
+          Travel with convenience
+        </div>
+        <div className="logo">
+          <div className="ma3pass2">
+            <span>
+              <span className="ma3pass2-span">Ma3</span>
+              <span className="ma3pass2-span2">Pass</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="bg3"></div>
+      <div className="group-66">
+        <div className="ellipse-35"></div>
+        <div className="ellipse-36"></div>
+      </div>
+      <div className="group-67">
+        <div className="rectangle-176"></div>
+        <div className="rectangle-177"></div>
+      </div>
+      <div className="group-457">
+        <div className="bar">
+          <div className="mail">
+            <div className="rectangle-4"></div>
+            <div className="input-your-email---">Input your email...</div>
+          </div>
+          <div className="button2">
+            <div className="rectangle-5"></div>
+            <div className="try-for-free">Try for free</div>
+          </div>
+        </div>
+        <div className="texts4">
+          <div className="get-notified-of-new-routes-and-discounts-on-the-go">
+            Get notified of new routes and discounts on the go
+          </div>
+          <div className="subscribe-our-newsletter">
+            Subscribe our newsletter
+          </div>
+        </div>
+      </div>
+      <div className="texts5">
+        <div className="greenline-coaches">Greenline Coaches</div>
+      </div>
+      <div className="line-4"></div>
+      <div className="texts">
+        <div className="ksh-2-000">Ksh 2,000</div>
+      </div>
+      <div className="image-17"></div>
+    </div>
+  );
+};
---
+++ src/pages/tickets.js
@@ -0,0 +1,240 @@
+import "./Tickets.css";
+
+export const Tickets = ({ ...props }) => {
+  return (
+    <div className="tickets">
+      <div className="bg"></div>
+      <div className="image-1"></div>
+      <div className="ellipse-733"></div>
+      <div className="ellipse-734"></div>
+      <div className="check-previous-and-current-tickets">
+        Check previous and current tickets
+      </div>
+      <div className="logo">
+        <div className="ma3pass">
+          <span>
+            <span className="ma3pass-span">ma3</span>
+            <span className="ma3pass-span2">Pass</span>
+          </span>
+        </div>
+      </div>
+      <div className="menu">
+        <div className="rectangle-121"></div>
+        <div className="my-tickets">My Tickets</div>
+        <div className="how-it-works">How it Works</div>
+        <div className="home">Home</div>
+        <div className="routes">Routes</div>
+      </div>
+      <div className="button">
+        <div className="rectangle-9"></div>
+        <div className="my-account">My Account</div>
+      </div>
+      <div className="rectangle-180"></div>
+      <div className="texts">
+        <div className="ticket-no--035672391">TICKET NO: 035672391</div>
+      </div>
+      <div className="nairobi">Nairobi</div>
+      <div className="mombasa">Mombasa</div>
+      <svg
+        className="arrow-6"
+        width="128"
+        viewBox="0 0 128 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M129.061 1.06066C129.646 0.474874 129.646 -0.474874 129.061 -1.06066L119.515 -10.6066C118.929 -11.1924 117.979 -11.1924 117.393 -10.6066C116.808 -10.0208 116.808 -9.07107 117.393 -8.48528L125.879 0L117.393 8.48528C116.808 9.07107 116.808 10.0208 117.393 10.6066C117.979 11.1924 118.929 11.1924 119.515 10.6066L129.061 1.06066ZM0 1.5L128 1.5V-1.5L0 -1.5L0 1.5Z"
+          fill="#9085D1"
+        />
+      </svg>
+      <div className="easycoach-buses">EasyCoach Buses</div>
+      <div className="ksh-1500">Ksh 1500</div>
+      <div className="rectangle-181"></div>
+      <div className="texts">
+        <div className="ticket-no--0356723912">TICKET NO: 035672391</div>
+      </div>
+      <div className="nairobi2">Nairobi</div>
+      <div className="mombasa2">Mombasa</div>
+      <svg
+        className="arrow-7"
+        width="128"
+        viewBox="0 0 128 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M129.061 1.06066C129.646 0.474874 129.646 -0.474874 129.061 -1.06066L119.515 -10.6066C118.929 -11.1924 117.979 -11.1924 117.393 -10.6066C116.808 -10.0208 116.808 -9.07107 117.393 -8.48528L125.879 0L117.393 8.48528C116.808 9.07107 116.808 10.0208 117.393 10.6066C117.979 11.1924 118.929 11.1924 119.515 10.6066L129.061 1.06066ZM0 1.5L128 1.5V-1.5L0 -1.5L0 1.5Z"
+          fill="#9085D1"
+        />
+      </svg>
+      <div className="easycoach-buses2">EasyCoach Buses</div>
+      <div className="ksh-15002">Ksh 1500</div>
+      <div className="rectangle-184"></div>
+      <div className="texts">
+        <div className="ticket-no--0356723913">TICKET NO: 035672391</div>
+      </div>
+      <div className="nairobi3">Nairobi</div>
+      <div className="mombasa3">Mombasa</div>
+      <svg
+        className="arrow-10"
+        width="128"
+        viewBox="0 0 128 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M129.061 1.06066C129.646 0.474874 129.646 -0.474874 129.061 -1.06066L119.515 -10.6066C118.929 -11.1924 117.979 -11.1924 117.393 -10.6066C116.808 -10.0208 116.808 -9.07107 117.393 -8.48528L125.879 0L117.393 8.48528C116.808 9.07107 116.808 10.0208 117.393 10.6066C117.979 11.1924 118.929 11.1924 119.515 10.6066L129.061 1.06066ZM0 1.5L128 1.5V-1.5L0 -1.5L0 1.5Z"
+          fill="#9085D1"
+        />
+      </svg>
+      <div className="easycoach-buses3">EasyCoach Buses</div>
+      <div className="ksh-15003">Ksh 1500</div>
+      <div className="rectangle-185"></div>
+      <div className="texts">
+        <div className="ticket-no--0356723914">TICKET NO: 035672391</div>
+      </div>
+      <div className="nairobi4">Nairobi</div>
+      <div className="mombasa4">Mombasa</div>
+      <svg
+        className="arrow-11"
+        width="128"
+        viewBox="0 0 128 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M129.061 1.06066C129.646 0.474874 129.646 -0.474874 129.061 -1.06066L119.515 -10.6066C118.929 -11.1924 117.979 -11.1924 117.393 -10.6066C116.808 -10.0208 116.808 -9.07107 117.393 -8.48528L125.879 0L117.393 8.48528C116.808 9.07107 116.808 10.0208 117.393 10.6066C117.979 11.1924 118.929 11.1924 119.515 10.6066L129.061 1.06066ZM0 1.5L128 1.5V-1.5L0 -1.5L0 1.5Z"
+          fill="#9085D1"
+        />
+      </svg>
+      <div className="easycoach-buses4">EasyCoach Buses</div>
+      <div className="ksh-15004">Ksh 1500</div>
+      <div className="rectangle-182"></div>
+      <div className="texts">
+        <div className="ticket-no--0356723915">TICKET NO: 035672391</div>
+      </div>
+      <div className="nairobi5">Nairobi</div>
+      <div className="thur--5th-nov-2022-at-7-30-am">
+        Thur, 5th Nov 2022 at 7:30 am
+      </div>
+      <div className="thur--5th-nov-2022-at-7-30-am2">
+        Thur, 5th Nov 2022 at 7:30 am
+      </div>
+      <div className="thur--5th-nov-2022-at-7-30-am3">
+        Thur, 5th Nov 2022 at 7:30 am
+      </div>
+      <div className="mombasa5">Mombasa</div>
+      <svg
+        className="arrow-8"
+        width="128"
+        viewBox="0 0 128 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M129.061 1.06066C129.646 0.474874 129.646 -0.474874 129.061 -1.06066L119.515 -10.6066C118.929 -11.1924 117.979 -11.1924 117.393 -10.6066C116.808 -10.0208 116.808 -9.07107 117.393 -8.48528L125.879 0L117.393 8.48528C116.808 9.07107 116.808 10.0208 117.393 10.6066C117.979 11.1924 118.929 11.1924 119.515 10.6066L129.061 1.06066ZM0 1.5L128 1.5V-1.5L0 -1.5L0 1.5Z"
+          fill="#9085D1"
+        />
+      </svg>
+      <div className="easycoach-buses5">EasyCoach Buses</div>
+      <div className="ksh-15005">Ksh 1500</div>
+      <div className="rectangle-183"></div>
+      <div className="texts">
+        <div className="ticket-no--0356723916">TICKET NO: 035672391</div>
+      </div>
+      <div className="nairobi6">Nairobi</div>
+      <div className="mombasa6">Mombasa</div>
+      <svg
+        className="arrow-9"
+        width="128"
+        viewBox="0 0 128 0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M129.061 1.06066C129.646 0.474874 129.646 -0.474874 129.061 -1.06066L119.515 -10.6066C118.929 -11.1924 117.979 -11.1924 117.393 -10.6066C116.808 -10.0208 116.808 -9.07107 117.393 -8.48528L125.879 0L117.393 8.48528C116.808 9.07107 116.808 10.0208 117.393 10.6066C117.979 11.1924 118.929 11.1924 119.515 10.6066L129.061 1.06066ZM0 1.5L128 1.5V-1.5L0 -1.5L0 1.5Z"
+          fill="#9085D1"
+        />
+      </svg>
+      <div className="easycoach-buses6">EasyCoach Buses</div>
+      <div className="ksh-15006">Ksh 1500</div>
+      <div className="group-17558">
+        <div className="bg2"></div>
+        <div className="group-17556">
+          <div className="ellipse-7332"></div>
+          <div className="ellipse-7342"></div>
+        </div>
+      </div>
+      <div className="rectangle-179"></div>
+      <div className="term---condition">Term &amp; Condition</div>
+      <div className="privacy---policy">Privacy &amp; Policy</div>
+      <div className="ma3pass-2022">MA3PASS 2022</div>
+      <div className="menu-3">
+        <div className="social">Social</div>
+        <div className="instagram">Instagram</div>
+        <div className="facebook">Facebook</div>
+        <div className="twitter">Twitter</div>
+        <div className="linkedin">Linkedin</div>
+      </div>
+      <div className="menu-1">
+        <div className="menu2">Menu</div>
+        <div className="home2">Home</div>
+        <div className="how-it-works2">How it works</div>
+        <div className="routes2">Routes</div>
+        <div className="my-tickets2">My Tickets</div>
+      </div>
+      <div className="group-458">
+        <div className="search--book-and-pay-for-your-matatu-ticket-the-easy-way-with-ma3pass--travel-with-convenience">
+          Search, book and pay for your matatu ticket the easy way with Ma3Pass.
+          Travel with convenience
+        </div>
+        <div className="logo">
+          <div className="ma3pass2">
+            <span>
+              <span className="ma3pass2-span">Ma3</span>
+              <span className="ma3pass2-span2">Pass</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="bg3"></div>
+      <div className="group-66">
+        <div className="ellipse-35"></div>
+        <div className="ellipse-36"></div>
+      </div>
+      <div className="group-67">
+        <div className="rectangle-176"></div>
+        <div className="rectangle-177"></div>
+      </div>
+      <div className="group-457">
+        <div className="bar">
+          <div className="mail">
+            <div className="rectangle-4"></div>
+            <div className="input-your-email---">Input your email...</div>
+          </div>
+          <div className="button2">
+            <div className="rectangle-5"></div>
+            <div className="try-for-free">Try for free</div>
+          </div>
+        </div>
+        <div className="texts2">
+          <div className="get-notified-of-new-routes-and-discounts-on-the-go">
+            Get notified of new routes and discounts on the go
+          </div>
+          <div className="subscribe-our-newsletter">
+            Subscribe our newsletter
+          </div>
+        </div>
+      </div>
+      <div className="thur--5th-nov-2022-at-7-30-am4">
+        Thur, 5th Nov 2022 at 7:30 am
+      </div>
+      <div className="thur--5th-nov-2022-at-7-30-am5">
+        Thur, 5th Nov 2022 at 7:30 am
+      </div>
+      <div className="thur--5th-nov-2022-at-7-30-am6">
+        Thur, 5th Nov 2022 at 7:30 am
+      </div>
+    </div>
+  );
+};
