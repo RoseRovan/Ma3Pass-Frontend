ad4078af1707c756a235dddd8a4fbe07080d30e3
Sam Arita <arita.sam@student.moringaschool.com>
2022-11-10 09:37:36 +0300

    working on routes

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+import {Route, Routes} from "react-router-dom"
+import { LandingPage } from './landing';
+import { SelectBus } from './selectbus';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element = {<LandingPage/>}/>
+        <Route path='./selectbus/*' element= {<SelectBus/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
